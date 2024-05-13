023b99c4f6f734275db603e7a0bd53d7dbc4007d
johnfcoltrane <hy.p901is@gmail.com>
2024-05-13 22:08:34 +0900

    Add files via upload

--- libWStr.cpp
+++ libWStr.cpp
@@ -16,19 +16,55 @@ static std::wstring ConvertUTF8ToWstring(const std::string& src)
 }
 
 extern "C"
-void str2wstr(wchar_t* q, int n, const char* p) 
+size_t str2wstr(wchar_t* q, int n, const char* p) 
 {
 	std::string src(p);
 	std::wstring wstr = ConvertUTF8ToWstring(src);
 	const wchar_t* qq = wstr.c_str();
 	wcscpy_s(q, n, qq);
+	size_t m = wcslen(q);
+	return(m * sizeof(*q));
 }
 
 extern "C"
-void wstr2str(char* p, int n, const wchar_t* q) 
+size_t wstr2str(char* p, int n, const wchar_t* q) 
 {
 	std::wstring src(q);
 	std::string str = ConvertWstringToUTF8(src);
 	const char* qq = str.c_str();
 	strcpy_s(p, n, qq);
+	size_t  m = strlen(p);
+	return(m * sizeof(*p));
+}
+
+/*
+	wstr -> str
+	void a[]を使用
+	void* ap = &a;
+	return: 次のポインタ位置
+	void* convWstr2Str(char* p, wchar_t* q, void* ap);
+*/
+
+extern "C"
+void* convStr2Wstr(void* ap, const char* p)
+{
+	std::string src(p);
+	std::wstring wstr = ConvertUTF8ToWstring(src);
+	const wchar_t* qq = wstr.c_str();
+	size_t n = wcslen(qq);
+	wcscpy_s((wchar_t *)ap, n+1, qq);
+	//size_t m = wcslen(q);
+	return((wchar_t *)ap + n+1);
+}
+
+extern "C"
+void* convWstr2Str(void* ap, const wchar_t* q)
+{
+	std::wstring src(q);
+	std::string str = ConvertWstringToUTF8(src);
+	const char* qq = str.c_str();
+	size_t n = strlen(qq);
+	strcpy_s((char*)ap, n+1, qq);
+	//size_t  m = strlen(p);
+	return((char *)ap + n+1);
 }
--- libWStr.h
+++ libWStr.h
@@ -1,9 +1,10 @@
 #pragma once
-
+#if 0
 static std::string ConvertWstringToUTF8(const std::wstring& src);
 static std::wstring ConvertUTF8ToWstring(const std::string& src);
+#endif
 
 extern "C" {
-void str2wstr(wchar_t* q, int n, const char* p);
-void wstr2str(char* p, int n, const wchar_t* q);
+size_t str2wstr(wchar_t* q, int n, const char* p);
+size_t wstr2str(char* p, int n, const wchar_t* q);
 }
